b2750cdf15b5ae5ddeec33b43c0cc9f7dacf0c1a
wardDward <edward.taligatos03gmail.com>
2025-04-04 16:10:03 +0800

    register page

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -3,13 +3,13 @@ import Google from "../../assets/google.png";
 import Github from "../../assets/github.png";
 import { Link } from "react-router-dom";
 
-function Login() {
+function Register() {
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="w-full md:w-[50%] lg:w-[35%] xl:w-[25%] bg-white shadow-lg px-2 py-5 border border-slate-300 rounded-md">
         <div className="text-center">
           <h3 className="text-lg font-bold tracking-wide">Trello-Clone</h3>
-          <p className="my-2 text-md font-semibold">Log in to continue</p>
+          <p className="my-2 text-md font-semibold">Create an account to continue</p>
         </div>
         <div className="px-4 mt-5">
           <div>
@@ -19,30 +19,17 @@ function Login() {
                 placeholder="Enter your email"
                 className="border border-slate-400 py-[3px] px-2 w-full rounded-md placeholder:text-sm"
               />
-              {/* hide if email do not exist show if email exists */}
-              {/* <input
-                type="password"
-                placeholder="Enter your password"
-                className="border border-slate-400 py-[5px] px-2 w-full rounded-md mt-3 placeholder:text-sm"
-              /> */}
               <div className="mt-2">
                 <button
                   type="submit"
                   className="w-full bg-blue-700 py-1 text-white rounded-md font-[500] hover:bg-blue-600 cursor-pointer"
                 >
-                  Continue
+                  Register
                 </button>
-                {/* hide if email do not exist show if email exists */}
-                {/* <button
-                  type="submit"
-                  className="w-full bg-blue-700 py-1 text-white rounded-md font-[500] hover:bg-blue-600 cursor-pointer"
-                >
-                  Log In
-                </button> */}
               </div>
               {/* socials */}
               <div className="my-3 text-gray-700 text-center font-semibold">
-                Or continue with:
+                Sign up with:
               </div>
               <div className="flex items-center justify-center border-[1px] border-slate-400 py-1 rounded-md hover:bg-slate-100 cursor-pointer">
                 <img src={Google} className="h-[20px] w-[20px] mr-2" alt="" />
@@ -54,9 +41,9 @@ function Login() {
               </div>
             </form>
             <div className="flex items-center justify-center mt-3 text-sm text-blue-700">
-              <span> Can't log in? </span>
+              <span> Already have an account ? </span>
               <span className="mx-3 text-lg">•</span>
-              <Link to={"/register"} className="hover:underline">Create an account.</Link>
+              <Link to={"/login"} className="hover:underline">Log In.</Link>
             </div>
           </div>
         </div>
@@ -65,4 +52,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Register;
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 import Index from "../pages/Index";
 import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Index />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Route>
   )
 );
