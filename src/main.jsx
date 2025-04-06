d32ce6274c9a17ef4fccc1493b0a87677416b531
wardDward <edward.taligatos03@gmail.com>
2025-04-06 15:53:20 +0800

    axios defaults

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,12 @@ import { RouterProvider } from 'react-router-dom'
 import router from './router/routes.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import axios from 'axios'
+
+
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
