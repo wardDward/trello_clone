3932b4471fb0fccc649d29ddd5802333fd3417b7
wardDward <edward.taligatos03@gmail.com>
2025-04-09 12:16:18 +0800

    drop down component

---
+++ src/components/navs/DropDown.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef } from 'react';
+
+function DropDown({ id, name, openDropdownId, setOpenDropdownId, renderContent }) {
+  const dropdownRef = useRef(null);
+  const isOpen = openDropdownId === id;
+
+  const toggleDropdown = () => {
+    setOpenDropdownId(isOpen ? null : id);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpenDropdownId(null);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [setOpenDropdownId]);
+
+
+  return (
+    <div className="relative" ref={dropdownRef}>
+      <button onClick={toggleDropdown} className="py-1 px-2 hover:bg-slate-100 rounded-md">
+        {name}
+      </button>
+      {isOpen && (
+        <div className=" bg-white shadow-md mt-2 p-2 border border-slate-300 rounded-md absolute top-full">
+          {renderContent}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default DropDown;
