47ea727bec5417c0d72cc7b849aa577dcdc3134c
wardDward <edward.taligatos03@gmail.com>
2025-04-09 21:07:21 +0800

    feat: sizing and spacing

--- src/components/navs/Sidebar.jsx
+++ src/components/navs/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div className=''>
+    <div className='bg-white h-full hidden md:flex fixed top-[54px] w-full md:w-[25%] lg:w-[23%] xl:w-[15%]'>
       Sidebar
     </div>
   )
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -15,9 +15,9 @@ function DefaultLayout() {
         <>
             <Navbar />
             <div className='h-screen max-w-full bg-slate-100 flex justify-center mt-[50px]'>
-                <div className='flex w-full md:w-[80%] lg:w-[85%] xl:w-[70%] bg-red-50'>
+                <div className='flex w-full md:w-[80%] lg:w-[85%] xl:w-[70%]'>
                     <Sidebar />
-                    <div>
+                    <div className='ml-0 md:ml-[31.5%] lg:ml-[27.5%] xl:ml-[21.5%]'>
                         <Outlet />
                     </div>
                 </div>
